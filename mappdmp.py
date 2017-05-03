0600b7ef676bf1eb355e1acae9f5921e64228965
Jan Fait <blowinyourmind@seznam.cz>
2017-05-03 10:59:47 +0200

    second
    second

--- mappdmp.py
+++ mappdmp.py
@@ -34,12 +34,19 @@ class MappDmp:
        self.endpoints = {
        'auth':'/auth',
        'listexports':'/viz/list-exports',
-       'export':'/viz/export'
+       'export':'/viz/export',
+       'data':'/viz/data'
        }
        self.dictionary = {
-               'dimensions':['flx_event_type','flx_interaction_type','flx_pixel_id','flx_uuid','flx_segment_dmp','flx_conversion_dmp','flx_event_url','flx_timestamp','flx_date','flx_event_referrer_url']
-               'measures':['flx_interactions_dmp','flx_clicks_dmp','flx_impressions_dmp','flx_total_events_dmp']
+               'dimensions':['flx_interaction_timeonsite','flx_interaction_pagescroll','flx_event_type','flx_interaction_type','flx_pixel_id','flx_uuid','flx_segment_dmp','flx_conversion_dmp','flx_event_url','flx_timestamp','flx_date','flx_event_referrer_url'],
+               'measures':['flx_interactions_dmp','flx_clicks_dmp','flx_impressions_dmp','flx_total_events_dmp'],
+               'errors':{'export_ready':'This export is already'}
        }
+       self.defaults = {
+          'dimensions':['flx_event_type','flx_interaction_type','flx_pixel_id','flx_uuid','flx_segment_dmp','flx_conversion_dmp','flx_event_url','flx_timestamp','flx_date','flx_event_referrer_url'],
+          'measures':['flx_interactions_dmp'],      
+       }
+       self
        self.dprint('Initializing Mapp DMP API')
        if not username or not password:
            raise MissingCredentialsException(message='username and password attributes are required')
@@ -99,7 +106,7 @@ class MappDmp:
        headers = {'X-Auth': self.session['response']['token'],'X-CSRF':self.session['response']['csrf'],'Accept':'application/json'}
        return headers
        
-   def call(self,endpoint=None,method='GET',params=None,body=None,stream=False):
+   def call(self,endpoint=None,method='GET',params=None,body=None):
        url = self.build_url(endpoint)
        headers = self.build_headers()
        self.dprint('Calling URL',url)
@@ -108,14 +115,15 @@ class MappDmp:
        if not self.check_login:
            self.login()
        if method == 'GET':
-           response = requests.get(url=url,headers=headers,stream=stream)
-       else: 
-           response = requests.post(url=url,data=body,headers=headers,stream=stream)
+           response = requests.get(url=url,headers=headers)
+       if method == 'POST': 
+           response = requests.post(url=url,data=body,headers=headers)
            self.dprint('Sending request data',response.request.data)
        json = response.json()
        return json
        
    def list_exports(self):
+       self.dprint('Fetching current export list')
        data = self.call('listexports')
        return data
        
@@ -124,6 +132,7 @@ class MappDmp:
        headers = self.build_headers()
        params = {'id':export_id}
        tempfile = 'MappDmpExport_'+ str(export_id) + '.txt'
+       self.dprint('Fetching export',export_id,'to file',tempfile)
        if stream:
            r = requests.get(url,params=params,headers=headers,stream=stream)
            self.dprint('Calling URL',r.request.url)
@@ -140,36 +149,57 @@ class MappDmp:
           r = requests.get(url,params=params,headers=headers,stream=False)
           return r.Response.raw
     
-   def get_data(self,dimensions=None,measures=None,filters=None,limit=None,batch=False):
+   def get_data(self,dimensions=None,measures=None,filters=None,limit=None,batch=False,retry_period=10,add_defaults=False):
        dimensions = self.validate_dimensions(dimensions)
        measures = self.validate_measures(measures)
        filters = self.validate_filters(filters)
        query = self.prepare_query(dimensions,measures,filters,limit)
        
        if batch:
+           self.dprint('Running the batch export procedure')
            response = self.call(endpoint='batch',method='POST',body=query)
            if response['status'] == 'ERROR' and response['error'] == self.dictionary['errors']['export_ready']:
                export_id = response['id']
            elif response['status'] == 'OK':
                export_ready = False
                while not export_ready:
-                   time.sleep(10)
-                   export_status = self.check_export_status()
-                   if export_status == 'COMPLETED':
-                       export_ready = True
-                   data = self.get_export(export_id=export_id)
-                   return data
+                   time.sleep(retry_period)
+                   export_ready = self.is_export_ready(export_id=export_id)
+               data = self.get_export(export_id=export_id)
+               return data
            else:
                return response
   
        else:
+           self.dprint('Running the data query')
            response = self.call(endpoint='data',method='POST',body=query)
            return response
        
-   def check_export_status(export_id=None):
-       response = self.list_exports()
+   def is_export_ready(self,export_id=None):
+       self.dprint('Checking status of export',export_id)
+       data = self.list_exports()
+       if not data['response']['exports']:
+           return False
+       else:
+           exports = data['response']['exports']
+       for export in exports:
+        status = export['state']
+        self.dprint('Export',export_id,'status is',status)
+        id = export['id']
+        if status == 'COMPLETED' and id==export_id:
+            return True
+        else:
+            return False
+  
+   def parse_input(self,input):
+       if type(input) == 'str':
+           output = input.split(",")
+       if type(input) == 'list':
+           output = input
+       return output
        
-   def prepare_query(dimensions,measures,filters,limit):
+   def prepare_query(self,dimensions,measures,filters,limit):
+       self.dprint('Preparing query')
        query = {}
        query['dimensions'] = dimensions
        query['measures'] = measures
@@ -178,23 +208,31 @@ class MappDmp:
        return query
        
    def validate_measures(self,input=None):
+       self.dprint('Validating measures')
        if not input:
-           return self.dictionary['measures']
+           self.dprint('Measures not supplied, selecting default')
+           return self.defaults['measures']
        else:
+           input = self.parse_input(input)
            valid = self.dictionary['measures']
            output = input[input in valid]
            return output
    def validate_dimensions(self,input=None):
+      self.dprint('Validating dimensions')
       if not input:
-          return self.dictionary['dimensions']
+          self.dprint('Dimensions not supplied, selecting default')
+          return self.defaults['dimensions']
       else:
+          input = self.parse_input(input)
           valid = self.dictionary['dimensions']
           output = input[input in valid]
           return output
       
    def validate_filters(self,input):
+       self.dprint('Validating filters')
        if not input:
-           return self.dictionary['filters']
+           self.dprint('Filters not supplied, selecting default, last 1 day')
+           return self.defaults['filters']
        else:
            return input
       
